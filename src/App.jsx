7f3d07d8e029b68e0f44bb03b6cc7a769110d238
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-08-28 13:53:29 +0100

    added favicon in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -54,12 +54,12 @@ function Pizza({ pizzaObj }) {
   //if (propObj.soldOut) return <header />;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt="{props.pizzaObj.name}" />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
